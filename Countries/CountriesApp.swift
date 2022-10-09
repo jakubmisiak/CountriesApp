b696453f646c0c1b3cc7d8e89049bce2acb1780b
jakubmisiak <jakubmisiak@icloud.com>
2022-10-09 10:51:57 +0200

    Initial Commit

---
+++ Countries/CountriesApp.swift
@@ -0,0 +1,17 @@
+//
+//  CountriesApp.swift
+//  Countries
+//
+//  Created by Jakub Misiak on 09/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct CountriesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
